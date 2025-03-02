d85d69092b30cdb4d8fcf656f90f89f1b3428601
Jan Wirth <jottenwe@gmail.com>
2025-03-02 11:04:24 +0100

    Prep for release

--- react-use-transcribe-gladia/src/index.ts
+++ react-use-transcribe-gladia/src/index.ts
@@ -1,3 +1,4 @@
 export { useTranscribeMic } from "./useTranscribeMic";
 export type { UseTranscribeMicProps } from "./useTranscribeMic";
 export { type GladiaWsMessage } from "./gladia/client";
+export { MicTest } from "./Demo";
